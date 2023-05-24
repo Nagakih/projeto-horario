37455d2aae66b80335754a3f72ea5394a874d02b
Nagakih <brunnocraft4@gmail.com>
2023-05-24 16:16:40 -0300

    Add Project

---
+++ ex01/app.js
@@ -0,0 +1,22 @@
+function load(){
+    var mensagem = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hours = data.getHours()
+    var minutes = data.getMinutes()
+    mensagem.innerHTML = `${hours}:${minutes}`
+
+    if(hours >= 0 && hours < 12){
+        img.src = '/Exercicios/img/dia.png'
+        document.body.style.backgroundColor = '#4a95ccff'
+        mensagem.style.color = '#4a95ccff'
+    }else if(hours >= 12 && hours < 18){
+        img.src = '/Exercicios/img/tarde.png'
+        document.body.style.backgroundColor = '#fbaa6dff'
+        mensagem.style.color = '#fbaa6dff'
+    }else{
+        img.src = '/Exercicios/img/noite.png'
+        document.body.style.backgroundColor = '#39474bff'
+        mensagem.style.color = '#39474bff'
+    }
+}
